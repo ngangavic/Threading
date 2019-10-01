bf996f85d344944d647c7641befc435d5aea7c80
ngangavic <ngangavictor10@gmail.com>
2019-10-01 13:10:31 +0300

    word model

---
+++ app/src/main/java/com/example/threading/models/Word.java
@@ -0,0 +1,101 @@
+package com.example.threading.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Word implements Parcelable{
+
+    @PrimaryKey(autoGenerate = true)
+    private int uid;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "content")
+    private String content;
+
+    @ColumnInfo(name = "timestamp")
+    private String timestamp;
+
+    @Ignore
+    public Word(String title, String content, String timestamp) {
+        this.title = title;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public Word() {
+
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(uid);
+        dest.writeString(title);
+        dest.writeString(content);
+        dest.writeString(timestamp);
+    }
+
+    protected Word(Parcel in) {
+        uid = in.readInt();
+        title = in.readString();
+        content = in.readString();
+        timestamp = in.readString();
+    }
+
+    public static final Creator<Word> CREATOR = new Creator<Word>() {
+        @Override
+        public Word createFromParcel(Parcel in) {
+            return new Word(in);
+        }
+
+        @Override
+        public Word[] newArray(int size) {
+            return new Word[size];
+        }
+    };
+}
